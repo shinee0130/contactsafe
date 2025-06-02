22837184bacf799bf3b3de0f87bda100edad2716
shinee0130 <shinekhuu.p999@gmail.com>
2025-06-02 11:10:34 +0800

    remove comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:contactsafe/app.dart'; // Import your app widget
+import 'package:contactsafe/app.dart';
 
 void main() {
   runApp(const ContactSafeApp());
